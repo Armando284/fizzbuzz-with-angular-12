bb01ab9dc793b542c5560d2e44a1b8cd0734774a
Armando P. Tamayo <arman2p284@gmail.com>
2021-07-08 01:29:42 -0400

    Removing default content and styles, and adding basic layout.

---
+++ src/app/components/fizzbuzz/fizzbuzz.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fizzbuzz',
+  templateUrl: './fizzbuzz.component.html'
+})
+export class FizzbuzzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
